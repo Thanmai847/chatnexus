e7b1c0f5ce313a14b21c7ee705d6f5b62be8cd27
Thanmai847 <thanmaicv@gmail.com>
2026-02-11 17:06:08 +0530

    update profile enpoint added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 export const signup= async (req,res) => {
   const { fullName, email, password } = req.body;
   
@@ -81,4 +82,19 @@ export const login =async (req,res) => {
 export const logout = (_,res) => {
     res.cookie("jwt","",{maxAge:0});
     res.status(200).json({message: "Logged out successfully"});
+};
+
+export const updateProfile = async (req,res) => {
+  try{
+    const {profilePic} = req.body;
+    if(!profilePic) return res.status(400).json({message: "Profile picture is required"});
+
+    const userId = req.user._id;
+    const uploadedResponse = await cloudinary.uploader.upload(profilePic)
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadedResponse.secure_url}, {new: true});
+  } catch(error){
+      console.log("Error in update profile:", error);
+      res.status(500).json({message: "Internal Server error"});
+  }
 };
